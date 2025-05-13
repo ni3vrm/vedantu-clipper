7116d05fd3a9dae0972e82fab672e62388dc9b89
Nitin Verma <nitin@Nitins-MacBook-Pro.local>
2025-05-13 21:27:32 +0530

    Add Dockerfile and update package.json for deployment

---
+++ Dockerfile.js
@@ -0,0 +1,16 @@
+FROM node:18
+
+RUN apt-get update && \
+    apt-get install -y ffmpeg && \
+    curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp && \
+    chmod a+rx /usr/local/bin/yt-dlp
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+ENV PORT=3000
+CMD ["npm", "start"]
